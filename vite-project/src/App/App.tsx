d33f6da836eba7ac3b549cbf54a720b598ac1867
FinJex <maks.vasylovych12@gmail.com>
2026-05-18 15:39:14 +0200

    hw

--- vite-project/src/App/App.tsx
+++ vite-project/src/App/App.tsx
@@ -1,19 +1,43 @@
 import css from "./App.module.css";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useState } from "react";
 import { fetchNotes } from "../services/noteService";
+import { useDebouncedCallback } from 'use-debounce';
 import NoteList from "../NoteList/NoteList";
 import Pagination from "../Pagination/Pagination";
 import Modal from "../Modal/Modal";
 import NoteForm from "../NoteForm/NoteForm";
+import SearchBox from "../SearchBox/SearchBox";
 
 
 export default function App() {
+    const [inputValue, setInputValue] = useState("");
+    const [search, setSearch] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
+
+const updateSearchQuery = useDebouncedCallback(
+  (value: string) => {
+    setSearch(value);
+    setCurrentPage(1);
+  },
+  300
+);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = e.target.value;
+
+    setInputValue(value);
+
+    updateSearchQuery(value);
+  };
+
 const { data, isLoading, isError} = useQuery({
-    queryKey: ["notes", currentPage],
-    queryFn: () => fetchNotes("", currentPage),
+    queryKey: ["notes", currentPage, search],
+    queryFn: () => fetchNotes(search, currentPage),
+    placeholderData: keepPreviousData,
 });
   if (isLoading) {
     return <p>Loading...</p>;
@@ -27,7 +51,7 @@ const { data, isLoading, isError} = useQuery({
     return (
         <div className={css.app}>
 	<header className={css.toolbar}>
-		{/* Компонент SearchBox */}
+		{<SearchBox value={inputValue} onChange={handleChange} />}
 		{data && data.totalPages > 1 && (<Pagination totalPages={data.totalPages} page={currentPage} setPage={setCurrentPage} />)}
         {<button type="button" className={css.button} onClick={() => setIsModalOpen(true)}>Create note +</button>}
         {isModalOpen && (<Modal onClose={() => setIsModalOpen(false)}> <NoteForm onClose={() => setIsModalOpen(false)} /> </Modal>)}
